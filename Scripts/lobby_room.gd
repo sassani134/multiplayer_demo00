b28e934af273261b5e420ea7a82d973d938bd4b2
Samuel <sassani134@gmail.com>
2024-08-03 15:24:25 +0200

    Init lobby - Signals two buttons - onready var textedit

---
+++ Scripts/lobby_room.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var text_edit = $VBoxContainer/TextEdit
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_host_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_join_pressed():
+	pass # Replace with function body.
