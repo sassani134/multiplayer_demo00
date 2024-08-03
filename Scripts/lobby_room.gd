2595263327e883af8ba0ee669dc9ca702c86af69
Samuel <sassani134@gmail.com>
2024-08-03 17:05:58 +0200

    Add comments

--- Scripts/lobby_room.gd
+++ Scripts/lobby_room.gd
@@ -1,6 +1,28 @@
 extends Control
 
 @onready var text_edit = $VBoxContainer/TextEdit
+var peer := ENetMultiplayerPeer.new()
+@export var player_scene : PackedScene
+
+func del_palyer(id : int) -> void :
+	rpc("_del_player", id)
+	return
+	
+@rpc("any_peer","call_local")
+func _del_player(id : int) -> void:
+	get_node(str(id)).queue_free()
+
+func add_player(id : int = 1) -> void:
+	var player := player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player)
+	return
+
+func exit_game(id : int) -> void :
+	multiplayer.peer_disconnected.connect(del_palyer)
+	del_palyer(id)
+	return
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready()-> void:
@@ -13,8 +35,16 @@ func _process(delta : float) -> void:
 
 
 func _on_button_host_pressed() ->void:
-	pass # Replace with function body.
+	peer.create_server(1027)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
+	add_player()
+	# change scene
+	return
 
 
 func _on_button_join_pressed() ->void:
-	pass # Replace with function body.
+	peer.create_client("127.0.0.1", 1027)
+	multiplayer.multiplayer_peer = peer
+	# change scene
+	return
