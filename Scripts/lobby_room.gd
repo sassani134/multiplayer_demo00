7f0f1bd7adda31afb0113d3d3f96542c38bc5e10
Samuel <sassani134@gmail.com>
2024-08-03 15:32:51 +0200

    add typed

--- Scripts/lobby_room.gd
+++ Scripts/lobby_room.gd
@@ -3,18 +3,18 @@ extends Control
 @onready var text_edit = $VBoxContainer/TextEdit
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready()-> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta : float) -> void:
 	pass
 
 
-func _on_button_host_pressed():
+func _on_button_host_pressed() ->void:
 	pass # Replace with function body.
 
 
-func _on_button_join_pressed():
+func _on_button_join_pressed() ->void:
 	pass # Replace with function body.
