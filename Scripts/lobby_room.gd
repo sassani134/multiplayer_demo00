91af67ca08b7bf6f4f8993e885840e1d0342bea0
Samuel <sassani134@gmail.com>
2024-08-14 18:13:48 +0200

    Improve multiplayer code with some tuto

--- Scripts/lobby_room.gd
+++ Scripts/lobby_room.gd
@@ -1,16 +1,32 @@
 extends Control
-## These signals can be connected to by a UI lobby scene or the game scene.
-#signal player_connected(peer_id, player_info)
-#signal player_disconnected(peer_id)
-#signal server_disconnected
+
+# These signals can be connected to by a UI lobby scene or the game scene.
+signal player_connected(peer_id : int, player_info)
+signal player_disconnected(peer_id : int)
+signal server_disconnected
 
 const PORT = 7000
 const LOCALHOST = "127.0.0.1" # IPv4 localhost
 const MAX_CONNECTIONS = 20
 
+# This will contain player info for every player,
+# with the keys being each player's unique IDs.
+var players : Dictionary = {}
+
+# This is the local player info. This should be modified locally
+# before the connection is made. It will be passed to every other peer.
+# For example, the value of "name" can be set to something the player
+# entered in a UI scene.
+var player_info : Dictionary = {"name": "Name"}
+
+var players_loaded : int = 0
+
 @onready var text_edit = $VBoxContainer/TextEdit
 var peer := ENetMultiplayerPeer.new()
+
+# ¿ Ressources qui a une PackedScene image son ... pour une selection ?
 @export var player_scene : PackedScene
+@export var map_scene : PackedScene
 
 func del_palyer(id : int) -> void :
 	rpc("_del_player", id)
@@ -19,6 +35,7 @@ func del_palyer(id : int) -> void :
 @rpc("any_peer","call_local")
 func _del_player(id : int) -> void:
 	get_node(str(id)).queue_free()
+	return
 
 func add_player(id : int = 1) -> void:
 	## instantiate in the next scene
@@ -44,21 +61,36 @@ func _ready()-> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta : float) -> void:
-	pass
+	return
 
 
-func _on_button_host_pressed() ->void:
-	peer.create_server(1027)
+func _on_button_host_pressed():# ¿ retournez l'erreur ou rien ?
+	var error = peer.create_server(PORT, MAX_CONNECTIONS)#1027 port tuto prece
+	if error:
+		return error
 	multiplayer.multiplayer_peer = peer
+	#
 	multiplayer.peer_connected.connect(add_player)
 	add_player()
+	#essentielement la meme chose 
+	players[1] = player_info
+	player_connected.emit(1, player_info)
+
 	#get_tree().change_scene("res://Game.tscn")
 	return
 
 
-func _on_button_join_pressed() ->void:
-	peer.create_client("127.0.0.1", 1027)
+func _on_button_join_pressed() :# ¿ retournez l'erreur ou rien ?
+	var error = peer.create_client(LOCALHOST, PORT) #1027 port tuto prece
+	if error:
+		return error
 	multiplayer.multiplayer_peer = peer
 	#get_tree().change_scene("res://Game.tscn")
 	## je ne dois pas ajouter un player aussi ?
-	return
+	
+
+func empty_name() -> bool:
+	if text_edit.text == null:
+		return false
+	else:
+		return true
